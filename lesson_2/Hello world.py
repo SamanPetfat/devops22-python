5ead98bfc33a61921bddb981195996d40045d210
Saman Petfat <Saman.Petfat@yh-nackademin.se>
2022-09-01 15:26:06 +0200

    Numbers & string

---
+++ lesson_2/Hello world.py
@@ -0,0 +1,5 @@
+msg = "hello world"
+print(msg)
+print("who are you?")
+name = input("")
+print(f'hello, {name}')
---
+++ lesson_2/integers.py
@@ -0,0 +1,8 @@
+#Frist number
+print("the first number is", 2)
+
+#creating a integer, saving it into a string
+new_number = 3
+
+#calling the string.
+print ("the second number is ",new_number)
