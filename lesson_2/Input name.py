31fd94e65e69e5ec2be20bacda6b2df38bbf05b6
Saman Petfat <Saman.Petfat@yh-nackademin.se>
2022-09-02 10:23:50 +0200

    Input all excersizes

---
+++ lesson_2/Input name.py
@@ -0,0 +1,15 @@
+# Asks user to write name, Welcomes user and output name.
+name = input("Hello, Please write your name: ""\n")
+print("Welcome, ",name)
+
+# Asks user to input a number, and  output doubles Users value using the Operator
+int_value = int(input("Write a number: "))
+print(f"your chosen value  {int_value},"" will be doubled: ", int_value * 2)
+
+# Asks user to input some text, and outputs it twice
+a_string = input("write something funny and i will copy what you said: ""\n")
+print(a_string * 2)
+
+# Asks user to input a float number (Decimals), and output is divided that with 3.5
+a_float = float(input("write a float: "))
+print(f"Your float number will be divided by 3.5:  ", a_float / 3.5)
