2265c634ffe7e3dfc287da192d70d23c24d0b7b9
Saman Petfat <Saman.Petfat@yh-nackademin.se>
2022-09-02 09:34:23 +0200

    F-string&None

---
+++ lesson_2/F-string&None.py
@@ -0,0 +1,18 @@
+#Stores None in a variable., prints None"
+my_nothing = None
+print (my_nothing)
+
+#F-String Compilation training
+my_int = 2
+print(my_int)
+
+my_string = "hello world"
+print(my_string)
+
+my_float = 4.0
+print(my_float)
+
+my_bool = True
+print(my_bool)
+#This is all of them combined using a F-string method.
+print(f'{my_int}, {my_float}, {my_string}, {my_bool}, {my_nothing}')
