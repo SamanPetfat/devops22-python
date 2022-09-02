7825f52590891bf7ec6f18966f42843db4ebd600
Saman Petfat <Saman.Petfat@yh-nackademin.se>
2022-09-02 09:20:23 +0200

    Floats&Bools

---
+++ lesson_2/Floats&Bools.py
@@ -0,0 +1,7 @@
+#Stores a float value in a variable, prints the2455 value with decimals, i.e 4.0.
+my_floats = 4.0
+print(my_floats)
+
+#Store a Boolean value in a variable, prints True or false value.
+my_bool = False
+print(my_bool)
